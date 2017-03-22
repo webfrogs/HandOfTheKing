132961ab5d984eaf4ee07e181f157a90e0abcd64
Carl Chen <ccf.developer@gmail.com>
2017-03-22 20:01:29 +0800

    init

---
+++ HandOfTheKing/HandOfTheKing.h
@@ -0,0 +1,19 @@
+//
+//  HandOfTheKing.h
+//  HandOfTheKing
+//
+//  Created by Carl on 22/3/2017.
+//  Copyright © 2017 nswebfrog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HandOfTheKing.
+FOUNDATION_EXPORT double HandOfTheKingVersionNumber;
+
+//! Project version string for HandOfTheKing.
+FOUNDATION_EXPORT const unsigned char HandOfTheKingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HandOfTheKing/PublicHeader.h>
+
+
---
+++ README.md
@@ -0,0 +1,11 @@
+
+[![Carthage compatible](https://img.shields.io/badge/Carthage-compatible-4BC51D.svg?style=flat)](https://github.com/Carthage/Carthage)
+
+HandOfTheKing is a basic framework dependence environment for building iOS project. 
+
+Carthage support only. 
+
+It depends these frameworks:
+
+- RxSwift
+- SnapKit
